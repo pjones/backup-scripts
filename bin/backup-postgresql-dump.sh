5590c5f5b3c008ef5546b9eab0503207654eb601
Peter Jones <pjones@devalot.com>
2018-11-30 17:44:37 -0700

    su: Need the `-' flag to create the proper environment

--- bin/backup-postgresql-dump.sh
+++ bin/backup-postgresql-dump.sh
@@ -11,5 +11,5 @@ BACKUP_FILE_NAME="$(date +%Y-%m-%d_%H:%M:%S).xz"
 
 ################################################################################
 mkdir -p "$BACKUP_DIRECTORY"
-su postgres -c 'pg_dumpall' | xz > "$BACKUP_DIRECTORY/_$BACKUP_FILE_NAME"
+su - postgres -c 'pg_dumpall' | xz > "$BACKUP_DIRECTORY/_$BACKUP_FILE_NAME"
 mv "$BACKUP_DIRECTORY/_$BACKUP_FILE_NAME" "$BACKUP_DIRECTORY/$BACKUP_FILE_NAME"
