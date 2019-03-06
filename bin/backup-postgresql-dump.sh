dbdd629cf7f04d1f4490895ca88142fa826a5675
Peter Jones <pjones@devalot.com>
2019-03-06 12:06:47 -0700

    PostgreSQL: Add option to use pg_dump on a single database instead of using pg_dumpall

--- bin/backup-postgresql-dump.sh
+++ bin/backup-postgresql-dump.sh
@@ -10,6 +10,20 @@ BACKUP_DIRECTORY=${BACKUP_DIRECTORY:-/var/lib/backup/postgres}
 BACKUP_FILE_NAME="$(date +%Y-%m-%d_%H:%M:%S).xz"
 
 ################################################################################
+DUMPALL_OPTIONS=${DUMPALL_OPTIONS:-"-o"}
+DUMPONE_OPTIONS=${DUMPONE_OPTIONS:-"-Cbo"}
+DUMP_CMD="pg_dumpall"
+
+################################################################################
+# When given a name of a database, dump only that database:
+if [ $# -eq 1 ] && [ -n "$1" ]; then
+  BACKUP_DIRECTORY="$BACKUP_DIRECTORY/$1"
+  DUMP_CMD="pg_dump --dbname=$1"
+  DUMP_ARGS=$DUMPONE_OPTIONS
+else
+  DUMP_ARGS=$DUMPALL_OPTIONS
+fi
+
 mkdir -p "$BACKUP_DIRECTORY"
-su - postgres -c 'pg_dumpall' | xz > "$BACKUP_DIRECTORY/_$BACKUP_FILE_NAME"
+su - postgres -c "$DUMP_CMD $DUMP_ARGS" | xz > "$BACKUP_DIRECTORY/_$BACKUP_FILE_NAME"
 mv "$BACKUP_DIRECTORY/_$BACKUP_FILE_NAME" "$BACKUP_DIRECTORY/$BACKUP_FILE_NAME"
