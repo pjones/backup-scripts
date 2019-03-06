9cadcbeb970405ec99fe99304fcf2ece84927cf9
Peter Jones <pjones@devalot.com>
2019-03-06 14:20:57 -0700

    PostgreSQL: don't require root privileges to run a backup

--- bin/backup-postgresql-dump.sh
+++ bin/backup-postgresql-dump.sh
@@ -12,18 +12,24 @@ BACKUP_FILE_NAME="$(date +%Y-%m-%d_%H:%M:%S).xz"
 ################################################################################
 DUMPALL_OPTIONS=${DUMPALL_OPTIONS:-"-o"}
 DUMPONE_OPTIONS=${DUMPONE_OPTIONS:-"-Cbo"}
-DUMP_CMD="pg_dumpall"
+
+################################################################################
+command="pg_dumpall"
+args=()
 
 ################################################################################
 # When given a name of a database, dump only that database:
 if [ $# -eq 1 ] && [ -n "$1" ]; then
   BACKUP_DIRECTORY="$BACKUP_DIRECTORY/$1"
-  DUMP_CMD="pg_dump --dbname=$1"
-  DUMP_ARGS=$DUMPONE_OPTIONS
+  command="pg_dump"
+  args+=("--dbname=$1")
+  args+=("$DUMPONE_OPTIONS")
 else
-  DUMP_ARGS=$DUMPALL_OPTIONS
+  args+=("$DUMPALL_OPTIONS")
 fi
 
 mkdir -p "$BACKUP_DIRECTORY"
-su - postgres -c "$DUMP_CMD $DUMP_ARGS" | xz > "$BACKUP_DIRECTORY/_$BACKUP_FILE_NAME"
+
+log "PostgreSQL backup: $command ${args[@]}"
+"$command" "${args[@]}" | xz > "$BACKUP_DIRECTORY/_$BACKUP_FILE_NAME"
 mv "$BACKUP_DIRECTORY/_$BACKUP_FILE_NAME" "$BACKUP_DIRECTORY/$BACKUP_FILE_NAME"
--- lib/log.sh
+++ lib/log.sh
@@ -3,21 +3,21 @@ export BACKUP_LOG_DIR=${BACKUP_LOG_DIR:-/var/lib/backup/log}
 
 ################################################################################
 # Where we're going to keep logs and signature files.
-log_file=$BACKUP_LOG_DIR/$BACKUP_NAME.log
-log_dir=$(dirname "$log_file")
+if [ "$BACKUP_LOG_DIR" != "stdout" ] && [ ! -t 1 ]; then
+  log_file=$BACKUP_LOG_DIR/$BACKUP_NAME.log
+  log_dir=$(dirname "$log_file")
+  mkdir -p "$log_dir"
+  exec > "$log_file" 2>&1
+fi
 
 ################################################################################
-mkdir -p "$log_dir"
-echo "Starting backup" > "$log_file"
-exec 4>&1 # save STDOUT to FD 4 so we can restore in die()
-[ -t 1 ] || exec > "$log_file" 2>&1
+echo "Starting backup"
 
 ################################################################################
 die () {
-  exec 1>&4 # Redirect STDOUT back to the original STDOUT.
   echo "ERROR:" "$@"
   echo
-  cat "$log_file"
+  [ -n "$log_file" ] && [ -e "$log_file" ] && cat "$log_file"
   exit 1
 }
 
